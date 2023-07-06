45dd6245e49c8cf7d91d8601517658c9aa6a1d60
damarnadh <kesireddydamarnadh@gmail.com>
2023-07-06 09:53:49 +0530

    records

---
+++ PL-SQL/S7. CURSORS/Cursor.sql
@@ -0,0 +1,52 @@
+CREATE VIEW sales AS
+SELECT customer_id,
+       SUM(unit_price * quantity) total,
+       ROUND(SUM(unit_price * quantity) * 0.05) credit
+FROM order_items
+INNER JOIN orders USING (order_id)
+WHERE status = 'Shipped'
+GROUP BY customer_id;
+
+DECLARE
+  l_budget NUMBER := 1000000;
+   -- cursor
+  CURSOR c_sales IS
+  SELECT  *  FROM sales  
+  ORDER BY total DESC;
+   -- record    
+   r_sales c_sales%ROWTYPE;
+BEGIN
+
+  -- reset credit limit of all customers
+  UPDATE customers SET credit_limit = 0;
+
+  OPEN c_sales;
+
+  LOOP
+    FETCH  c_sales  INTO r_sales;
+    EXIT WHEN c_sales%NOTFOUND;
+
+    -- update credit for the current customer
+    UPDATE 
+        customers
+    SET  
+        credit_limit = 
+            CASE WHEN l_budget > r_sales.credit 
+                        THEN r_sales.credit 
+                            ELSE l_budget
+            END
+    WHERE 
+        customer_id = r_sales.customer_id;
+
+    --  reduce the budget for credit limit
+    l_budget := l_budget - r_sales.credit;
+
+    DBMS_OUTPUT.PUT_LINE( 'Customer id: ' ||r_sales.customer_id || 
+' Credit: ' || r_sales.credit || ' Remaining Budget: ' || l_budget );
+
+    -- check the budget
+    EXIT WHEN l_budget <= 0;
+  END LOOP;
+
+  CLOSE c_sales;
+END;
